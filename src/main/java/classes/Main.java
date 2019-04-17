935f4bd95bb5508c54d4406798e0992d7d022c6d
JuaniFilardo <jifilardo@gmail.com>
2019-04-17 15:15:18 -0300

    Modificado sorting en reverse.

--- src/main/java/classes/Main.java
+++ src/main/java/classes/Main.java
@@ -9,9 +9,10 @@ import com.google.gson.JsonObject;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.lang.reflect.Array;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Date;
 import java.util.logging.FileHandler;
 import java.util.logging.Logger;
@@ -82,6 +83,7 @@ public class Main {
                             Ordenador.sortArray(agencies);
                         } else {
                             Ordenador.sortArray(agencies);
+                            Collections.reverse(Arrays.asList(agencies));
                         }
                     }
                 }
